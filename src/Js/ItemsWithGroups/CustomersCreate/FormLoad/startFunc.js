9cdfd9d0b0aef56d469ce38d4b525ba70f8135bf
sharath0036 <ksharathbabu2@gmail.com>
2025-05-27 15:55:34 +0530

    Customers and Orders

---
+++ src/Js/ItemsWithGroups/CustomersCreate/FormLoad/startFunc.js
@@ -0,0 +1,10 @@
+import { StartFunc as StartFuncAddListeners } from "./AddListeners/startFunc.js";
+import { StartFunc as ToDataList } from "./ToDataList/startFunc.js";
+
+
+const StartFunc = async () => {
+    StartFuncAddListeners();
+    // ToDataList();
+};
+
+export { StartFunc };
